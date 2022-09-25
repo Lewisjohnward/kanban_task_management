fdfecadb15c93a24e00975fda8c947330d6e0f42
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-09-25 12:09:06 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 
 const PORT = process.env.PORT || 3005
+
+const app = express()
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
